911a56b8233b66916e108d5631ea2a9e774a25a4
Greg Allen <me@jga.me>
2016-02-15 11:58:32 -0800

    added logger

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const config = require('confi')();
 const Server = require('./lib/server');
 const Deploy = require('./lib/deploy');
 const GithubHook = require('./lib/github-hook');
+const Logger = require('./lib/logger');
 
-if (!config.username || !config.token  || !config.secret) {
+if (!config.username || !config.token || !config.secret) {
+  /*eslint-disable no-console*/
   console.log('must pass in github username, github token, secret');
+  /*eslint-enable no-console*/
   process.exit(1);
 }
 
-const deploy = new Deploy(config.repoPath, config.username, config.token);
-const githubHook = new GithubHook(config.secret);
+const logger = new Logger(config.logger);
+const deploy = new Deploy(config.repoPath, config.username, config.token, logger);
+const githubHook = new GithubHook(config.secret, logger);
 const server = new Server(config.port, deploy, githubHook);
 
 server.start((err) => {
   if (err) {
     throw err;
   }
+  logger.log(['server'], 'Server started');
 });
