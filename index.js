3d8ed773fa696101381732a84db3f6a85d0ab9ed
Greg Allen <greg@firstandthird.com>
2016-03-28 21:11:11 -0700

    hapi-logr for server

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ config.branchWhitelist = config.branchWhitelist ? config.branchWhitelist.split('
 const log = new Logger(config.logger);
 const deploy = new Deploy(config.env, config.repoPath, config.username, config.token, log);
 const githubHook = new GithubHook(config.secret, config.branchWhitelist, log);
-const server = new Server(config.port, deploy, config.secret, githubHook);
+const server = new Server(config.port, deploy, config.secret, githubHook, config.logger);
 
 server.start((err) => {
   if (err) {
