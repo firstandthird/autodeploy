d0cd5d02d3e42b7ca340074e3f67625f4f5ef433
Greg Allen <greg@firstandthird.com>
2016-10-28 11:28:10 -0700

    better error checking for update-checker

--- server-plugins/update-checker/index.js
+++ server-plugins/update-checker/index.js
@@ -2,14 +2,21 @@
 const wreck = require('wreck');
 const pkg = require('../../package');
 exports.register = function(server, options, next) {
-
   const url = options.url;
 
   const check = function() {
     wreck.get(url, {
       json: true
     }, (err, res, payload) => {
+      if (err) {
+        server.log(['update-checker', 'error'], err);
+        return;
+      }
       payload = JSON.parse(payload);
+      if (res.statusCode !== 200) {
+        server.log(['update-checker', 'error'], payload);
+        return;
+      }
       if (payload.version !== pkg.version) {
         server.log(['update-checker', 'notice', 'warning'], { message: 'Deploy is out of date', installedVersion: pkg.version, newestVersion: payload.version });
       }
